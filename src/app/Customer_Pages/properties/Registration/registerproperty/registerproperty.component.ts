520f50c0703d088bedb55fc5cc37c8d62da301b5
gaikwadGFS <kshitija.gaikwad@growingflowers-solutions.com>
2025-01-21 11:49:27 +0530

    property routing done

---
+++ src/app/Customer_Pages/properties/Registration/registerproperty/registerproperty.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registerproperty',
+  standalone: true,
+  imports: [],
+  templateUrl: './registerproperty.component.html',
+  styleUrl: './registerproperty.component.css'
+})
+export class RegisterpropertyComponent {
+
+}
--- src/app/Customer_Pages/properties/properties-routing.module.ts
+++ src/app/Customer_Pages/properties/properties-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadpropertiesComponent } from './UploadProperties/uploadproperties/uploadproperties.component';
+import { ViewpropertiesComponent } from './UploadProperties/viewproperties/viewproperties.component';
+import { RegisterpropertyComponent } from './Registration/registerproperty/registerproperty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'uploadProperties',
+    component:UploadpropertiesComponent
+  },
+  {
+    path:'viewUploadedProperties',
+    component:ViewpropertiesComponent
+  },
+  {
+    path:'registerProperties',
+    component:RegisterpropertyComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
