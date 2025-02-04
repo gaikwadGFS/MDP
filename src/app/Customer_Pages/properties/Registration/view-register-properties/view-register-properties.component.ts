94628dd79211e5c70d66c7d5a8d790dfa66c2838
gaikwadGFS <kshitija.gaikwad@growingflowers-solutions.com>
2025-02-04 12:14:41 +0530

    login changes

--- src/app/Customer_Pages/properties/Registration/view-register-properties/view-register-properties.component.ts
+++ src/app/Customer_Pages/properties/Registration/view-register-properties/view-register-properties.component.ts
@@ -1,25 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
-import { AvatarModule } from 'primeng/avatar';
-import { StyleClassModule } from 'primeng/styleclass';
-import { Sidebar } from 'primeng/sidebar';
+
 
 @Component({
   selector: 'app-view-register-properties',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule],
+  imports: [],
   templateUrl: './view-register-properties.component.html',
   styleUrl: './view-register-properties.component.css'
 })
 export class ViewRegisterPropertiesComponent {
 
-  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
-
-  closeCallback(e:any): void {
-      this.sidebarRef.close(e);
-  }
-
-  sidebarVisible: boolean = false;
+ 
 }
