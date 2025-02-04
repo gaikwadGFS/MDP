f67bb187808a2bba4ca9865718629589725f2bc7
akankshakhose1005 <akanksha.khose@growingflowers-solutions.com>
2025-02-04 17:13:28 +0530

    check-maharara done

--- src/app/Customer_Pages/properties/check-maha-rera/check-maha-rera.component.ts
+++ src/app/Customer_Pages/properties/check-maha-rera/check-maha-rera.component.ts
@@ -5,6 +5,8 @@ import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+// import { FormControl } from '@angular/forms'; 
+// import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-check-maha-rera',
@@ -14,15 +16,48 @@ import { CommonModule } from '@angular/common';
   styleUrl: './check-maha-rera.component.css'
 })
 export class CheckMahaReraComponent {
+  loading: boolean = false;
+  
   documents = [
-    { subject: 'Math', date: new Date(), uploadedFile: null },
-    { subject: 'Science', date: new Date(), uploadedFile: null },
-    { subject: 'History', date: new Date(), uploadedFile: null }
+    { subject: 'Maharashtra Real Estate Regulatory Authority (General) (Amendment) Regulations, 2025', date: new Date(), uploadedFile: null },
+    { subject: 'Maharashtra Real Estate Regulatory Authority ( General) (Amendment) Regulations, 2021', date: new Date(), uploadedFile: null },
+    { subject: 'Maharashtra Real Estate Regulatory Authority ( General) (Amendment) Regulations, 2024', date: new Date(), uploadedFile: null },
+    { subject: 'MahaRERA General (Second Amendment) Regulations 2019.	', date: new Date(), uploadedFile: null },
+    { subject: 'MahaRERA General (Amendment) Regulations 2017.	', date: new Date(), uploadedFile: null },
+    { subject: 'MahaRERA General Regulations 2017	', date: new Date(), uploadedFile: null },
+    { subject: 'Maharashtra Real Estate Regulatory Authority (Recruitment and Conditions of Service of Employees) Regulations 2017	', date: new Date(), uploadedFile: null }
+
   ];
 
   onUpload(event: any, doc: any) {
     const file = event.files[0];
     doc.uploadedFile = file;
   }
+
+
+
+  load(): void {
+    this.loading = true; // Start loading
+    console.log('Load method triggered');
+
+    // Simulate some asynchronous operation
+    setTimeout(() => {
+      this.loading = false; // Stop loading
+    }, 2000);
+  }
+
+
+  // myForm = new FormGroup({
+  //   search: new FormControl(''),
+  // });
+
+  // search() {
+  //   console.log('Searching for:', this.myForm.value.search);
+  // }
+
+  // resetForm() {
+  //   this.myForm.reset(); // Clears the input field
+  // }
+
 
 }
