82562d063e312a4ad6675dad324e7abe4a5fae3d
Prajakta-Badhan <prajakta.badhan@growingflowers-solutions.com>
2025-02-12 13:07:12 +0530

    most popular changes done

--- src/app/Customer_Pages/Core/Services/api.service.ts
+++ src/app/Customer_Pages/Core/Services/api.service.ts
@@ -1,14 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
 export class ApiService {
 
+  private dbUrl = 'assets/db.json'; // Adjust path as needed
+
+  constructor(private http: HttpClient) { }
+
+  // Fetch all properties
+  getProperties(): Observable<any> {
+    return this.http.get<any>(this.dbUrl);
+  }
+
   apiUrlStartPoint:string="http://localhost:3000/"
-  constructor(private http:HttpClient) { }
 
   customerRegistration(obj:any):Observable<any>{
    return this.http.post(`${this.apiUrlStartPoint}customerRegister`,obj);
@@ -21,6 +25,5 @@ export class ApiService {
   getAllCustomers():Observable<any>{
     return this.http.get(`${this.apiUrlStartPoint}customerRegister`)
   }
-
 
 }
